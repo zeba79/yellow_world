ebf79c5393f4b41d0a206f8e37fd54d71d315d65
ZEBAZE <zeba_antoine@yahoo.fr>
2024-01-31 00:44:27 +0100

    create ùbonjour on BonjourController.php

--- src/Controller/BonjourController.php
+++ src/Controller/BonjourController.php
@@ -9,10 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class BonjourController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function bonjour(): Response
+    public function index(): Response
     {
+        $bonjour = 'Yess papa Controller Man';
         return $this->render('bonjour/index.html.twig', [
-            'bonjour' => 'Yess Controller',
+            'bonjour' => $bonjour,
         ]);
     }
 }
